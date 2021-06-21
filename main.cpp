d2b8877700c623ed6858b2a7ebb2b7c79fd1365c
kondorski <thegreatvaultofknowledge@gmail.com>
2021-06-22 01:01:56 +0200

    menu stub, main tests

--- main.cpp
+++ main.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
+#include <vector>
+
+#include "Student.hpp"
+#include "menu.hpp"
+
 
 int main () {
-    
+    Student student1 ("Konrad", "Karkowski", 'm', 35, "86111702275", 100001 );
+    Student student2 ("Konrad", "Kar", 'm', 31, "86111702275", 100002 );
+
+    std::vector<Student> studentDb;
+
+    studentDb.push_back(student1);
+    studentDb.push_back(student2);
+
+    for(auto& el : studentDb){
+        std::cout << el.get_first_name_() << " | " 
+                  << el.get_last_name_() << " | " 
+                  << el.get_age_() << " | " 
+                  << el.get_gender_() << " | " 
+                  << el.get_pesel_() << " | " 
+                  << el.get_id_()
+                  << '\n';
+    }
+
 }
---
+++ menu.cpp
@@ -0,0 +1,19 @@
+#include "menu.hpp"
+
+
+void menu_choice(const int input){
+    switch(input){
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        default:
+            std::cout << "Please choose from available inputs 1-5" << '\n';
+    }
+}
