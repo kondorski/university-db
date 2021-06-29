c05cb21eafe082bb3b6bf1ff6404bd5c79335ab7
kondorski <thegreatvaultofknowledge@gmail.com>
2021-06-29 21:02:01 +0200

    displayMenu added texts for options

--- menu.cpp
+++ menu.cpp
@@ -1,5 +1,5 @@
 #include "menu.hpp"
-#include "Student.hpp"
+
 
 std::vector<Student> universityDb;
 
@@ -14,14 +14,22 @@ void printDatabase (std::vector<Student>& vec) {
                   << el.get_id_()
                   << '\n';
     }
+
 }
 
+void addStudent (std::string first_name, std::string last_name, char gender, int age, std::string pesel, int id) {
+    universityDb.push_back(Student(first_name, last_name, gender, age, pesel, id));
+}
+
+
 void displayMenu() {
-    std::cout << "Press 1 to add student to the database" << '\n';
-    std::cout << "Press 2 to add student to delete " << '\n';
-    std::cout << "Press 3 to add student to delete " << '\n';
-    std::cout << "Press 4 to add student to delete " << '\n';
-    std::cout << "Press 5 to add student to delete " << '\n';
+    std::cout << "Press 1 to print all students in the database." << '\n';
+    std::cout << "Press 2 to add student in the database. " << '\n';
+    std::cout << "Press 3 to delete student from the database." << '\n';
+    std::cout << "Press 4 to search student by last name." << '\n';
+    std::cout << "Press 5 to search student by PESEL number." << '\n';
+    std::cout << "Press 6 to sort student by PESEL number." << '\n';
+    std::cout << "Press 7 to sort student by index number." << '\n';
 }
 
 void menu_choice(const int input) {
@@ -30,12 +38,16 @@ void menu_choice(const int input) {
             printDatabase(universityDb);
             break;
         case 2:
+            std::cout << "Pressed 2";
             break;
         case 3:
+            std::cout << "Pressed 3";
             break;
         case 4:
+            std::cout << "Pressed 4";
             break;
         case 5:
+            std::cout << "Pressed 5";
             break;
         default:
             std::cout << "Please choose from available inputs 1-5" << '\n';
--- menu.hpp
+++ menu.hpp
@@ -3,4 +3,9 @@
 #include <iostream>
 #include <vector>
 
+#include "Student.hpp"
+
 void menuInput (const int);
+void displayMenu();
+void printDatabase (std::vector<Student> &);
+void menu_choice(const int input);
